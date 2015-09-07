b17b4a7ece374055643558b62576f4167cddc42b
Matt Pagan <matthew.pagan@me.com>
2015-09-07 00:37:57 -0400

    Checkpoint complete, Seeding-data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,32 @@
 require 'faker'
 
-member = User.new(
-name:     'Member User',
-email:    'member@example.com',
-password: 'helloworld'
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+# Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+   )
+ member.skip_confirmation!
+ member.save!
+
+# Create an item
+item = Item.new(
+name:     'Default to-do',
 )
-member.skip_confirmation!
-member.save!
+item.save!
 
 puts "Seed finished"
 puts "#{User.count} users created"
+puts "#{Item.count} items created"
